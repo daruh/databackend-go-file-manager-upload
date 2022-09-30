0a27abff2f8876cd3d0cc9b37334171e02f4a0b7
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2022-09-30 12:49:18 +0200

    file upload spike: new client

--- multipartrelated/client.go
+++ multipartrelated/client.go
@@ -17,9 +17,18 @@ import (
 	"time"
 )
 
-type SnowClaims struct {
-	tenant string `json:"tenant"`
-	jwt.StandardClaims
+type SaveFile struct {
+	FileName   string            `json:"fileName"`
+	TenantId   string            `json:"tenantId"`
+	Mime       string            `json:"mime"`
+	Category   string            `json:"category"`
+	FileType   string            `json:"fileType"`
+	User       string            `json:"user"`
+	DeleteMark bool              `bson:"deleteMark"`
+	ExpiryTs   int64             `bson:"expiryTs"`
+	App        string            `json:"app"`
+	Size       int64             `json:"size"`
+	Tags       map[string]string `json:"tags"`
 }
 
 func main() {
@@ -63,26 +72,21 @@ func main() {
 		}
 	}
 
-	bearer := "eyJhbGciOiJSUzI1NiIsImtpZCI6IkRFNDBBMjU5MjRENDk1MERGMTkxNkI3RjlDMkJEOThBNkE4NzE1OEMiLCJ4NXQiOiIza0NpV1NUVWxRM3hrV3RfbkN2WmltcUhGWXciLCJ0eXAiOiJhdCtqd3QifQ.eyJhdXRoX3RpbWUiOjE2NjQ0MzYxOTQsInN1YiI6ImFlODYyNWE0LTRiZDAtNDc4Zi05ZTdiLTA4ZGEzMTg1ODdiNSIsInRlbmFudCI6ImNkYWZlOGRkLTlkZDItNDY0Ny0zNDM1LTA4ZGEzMTg1ODc5MCIsInJlZ2lvbiI6Im5vcnRoZXVyb3BlIiwiYWNjIjoiMDk2YjgxZTUtOGRlMS00NDI2LTI0ZDYtMDhkYTMxODU4N2I4IiwiZW1wbF90eXBlIjoicm5kIiwicm9sZSI6WyJzYWxlcy5tYW5hZ2VyIiwiZmlsZW1hbmFnZXIucm9sZSIsImlkLmlkcC5hZG1pbiJdLCJvaV9wcnN0IjoiMTVmNjQ4OWEtMjc0NS00NWJlLTkwMzctZmE5NzJiZGQzYjAxIiwiY2xpZW50X2lkIjoiMTVmNjQ4OWEtMjc0NS00NWJlLTkwMzctZmE5NzJiZGQzYjAxIiwidG9rX2lkIjoiMDcwMTFjY2UtNjhiMC00NGIyLThkMGEtYzE2NjIyNzU4ZGI5IiwiYXVkIjpbImFwaTovL3Nub3dzb2Z0d2FyZS5pby9hcGkiLCJhcGk6Ly9zbm93c29mdHdhcmUuaW8vaWRwL2FwaSJdLCJzY29wZSI6ImxpY2Vuc2luZy5saWNlbnNlLmNydWQgZWRnZS5pbnYuYXBpLmNydWQgaWQudGVuYW50cy5jcnVkIGZpbGVtYW5hZ2VyLnIgZmlsZW1hbmFnZXIuY3J1ZCBpZC51c2Vycy5jcnVkIGlkLmFwcHMuY3J1ZCBpZC5jbGFpbXMuY3J1ZCBpZC5vcmdhbml6YXRpb25hbHVuaXRzLmNydWQgaWQuc3lzLnRyc2MgaWQuc3lzLmNydWQgaWQubG9naW5wcm92aWRlcnMuY3J1ZCBpZC5yb2xlcy5jcnVkIG9wZW5pZCIsImV4cCI6MTY2NDQzOTc5NCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MTE4L2lkcCIsImlhdCI6MTY2NDQzNjE5NH0.ZA5-8fxPy4LdIbVhIq7scFWJHvVvqzdG9LNSezN1eYLUM28jWMXPHuwKBYHNdY-P2wSAiGu-9K9xlPDHBA8TCUpdhgEVLCTaX8xBkvquvctvDk_fwIamOdYpbZj5jkhxyA4ijQjDIu1DN79kLhAoR3cRtgKfSJmKEBugVOi0TWxAZGR9lwrIOf2iJk_mRlaiwoeJ1-2Ok6byvz2pgJF_4AR20dZYOrj_ux8pEvPJVEGD_eiLqlivC28U4sXFS4myda3RZMiYUksn-GgrLlwpgLGX-GbCHc8bwDEwjvyPqARMN3OIfePwUW_ZcQ_bPkqSBpL2pu0zSBcF1kOpJkTzQA"
+	bearer := "eyJhbGciOiJSUzI1NiIsImtpZCI6IkNDMjI1RjFGOTAxMzMzNkVBMzg0REQ1NjgwRDY5NjQyODI2MzA1MkEiLCJ4NXQiOiJ6Q0pmSDVBVE0yNmpoTjFXZ05hV1FvSmpCU28iLCJ0eXAiOiJhdCtqd3QifQ.eyJhdXRoX3RpbWUiOjE2NjQ1MjUzNjksInN1YiI6ImFlODYyNWE0LTRiZDAtNDc4Zi05ZTdiLTA4ZGEzMTg1ODdiNSIsInRlbmFudCI6ImNkYWZlOGRkLTlkZDItNDY0Ny0zNDM1LTA4ZGEzMTg1ODc5MCIsInJlZ2lvbiI6Im5vcnRoZXVyb3BlIiwiYWNjIjoiMDk2YjgxZTUtOGRlMS00NDI2LTI0ZDYtMDhkYTMxODU4N2I4IiwiZW1wbF90eXBlIjoicm5kIiwicm9sZSI6WyJzYWxlcy5tYW5hZ2VyIiwiZmlsZW1hbmFnZXIucm9sZSIsImlkLmlkcC5hZG1pbiJdLCJvaV9wcnN0IjoiMTVmNjQ4OWEtMjc0NS00NWJlLTkwMzctZmE5NzJiZGQzYjAxIiwiY2xpZW50X2lkIjoiMTVmNjQ4OWEtMjc0NS00NWJlLTkwMzctZmE5NzJiZGQzYjAxIiwidG9rX2lkIjoiMGVhMTJmNzktODlmYS00ZTdhLWI5NzktZTM4YTRiM2JkM2Q2IiwiYXVkIjpbImFwaTovL3Nub3dzb2Z0d2FyZS5pby9hcGkiLCJhcGk6Ly9zbm93c29mdHdhcmUuaW8vaWRwL2FwaSJdLCJzY29wZSI6ImxpY2Vuc2luZy5saWNlbnNlLmNydWQgZWRnZS5pbnYuYXBpLmNydWQgaWQudGVuYW50cy5jcnVkIGZpbGVtYW5hZ2VyLnIgZmlsZW1hbmFnZXIuY3J1ZCBpZC51c2Vycy5jcnVkIGlkLmFwcHMuY3J1ZCBpZC5jbGFpbXMuY3J1ZCBpZC5vcmdhbml6YXRpb25hbHVuaXRzLmNydWQgaWQuc3lzLnRyc2MgaWQuc3lzLmNydWQgaWQubG9naW5wcm92aWRlcnMuY3J1ZCBpZC5yb2xlcy5jcnVkIG9wZW5pZCIsImV4cCI6MTY2NDUyODk3MCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MTE4L2lkcCIsImlhdCI6MTY2NDUyNTM3MH0.A6eZhN6lATdzAGkT85IUbP58GV5DLSXhbVeacRod29wMANeZIB5KduWGDz0yXEcxMcBKYsoX5hPWeXjo7SlvwaIdpu73rCGyJZ88dF6rEiZqS1b02vYa8DgsasPH_ZaE2T-Z2kOLnMsMSqba10_UVVMNWtMWLnOLftR8bmpy03TbBhCQk6uR7DPnTJ_5G3d_KF57qOUaCEp-Y8PyF2x_kpuATeQum3Olc-qAJ82BJZFSMqQ749j_vhLNps6SL7IZPKHH1NEb1I4NzpDBEASdKniGNEpAiu_jHOWzpqf1oF3yBVWs-7Bu3btIWLOSRcF1nEtggZXaBkhyil0a3e0erg"
 
-	var claims SnowClaims
-	token, err := jwt.ParseWithClaims(bearer, claims, func(token *jwt.Token) (interface{}, error) {
+	token, _ := jwt.Parse(bearer, func(token *jwt.Token) (interface{}, error) {
 		return nil, nil
 	})
 
-	if err != nil {
-		panic(err)
-	}
-
-	parsedClaims := token.Claims.(*SnowClaims)
+	parsedClaims := token.Claims.(jwt.MapClaims)
 
 	faker := faker.NewFaker()
 	metadata := SaveFile{
 		FileName:   faker.RandomFileName() + ".pdf",
 		Mime:       "application/pdf",
 		DeleteMark: false,
-		TenantId:   parsedClaims.tenant,
-		User:       parsedClaims.Audience,
+		TenantId:   parsedClaims["tenant"].(string),
+		User:       parsedClaims["sub"].(string),
 		Size:       size,
 		Category:   faker.RandomProductAdjective(),
 		FileType:   faker.RandomFileType(),
@@ -121,7 +125,7 @@ func main() {
 	urls["prod"] = "https://westeurope.dev-snowsoftware.io"
 	urls["dev"] = "http://localhost:8008"
 
-	uploadURL := urls[env] + "/api/filemanager/tenants/" + parsedClaims.tenant + "/upload"
+	uploadURL := urls[env] + "/api/filemanager/tenants/" + parsedClaims["tenant"].(string) + "/upload"
 
 	r, err := http.NewRequest(http.MethodPost, uploadURL, bytes.NewReader(body.Bytes()))
 	if err != nil {
